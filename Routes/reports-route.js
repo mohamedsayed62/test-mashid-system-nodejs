d2b5d86fb8b4191a034e541c9babef625f94dfdb
MohamedAbdeltwab401 <118615015+mohamedsayed62@users.noreply.github.com>
2026-08-05 00:09:09 +0300

    Update reports-route.js

--- Routes/reports-route.js
+++ Routes/reports-route.js
@@ -2,7 +2,7 @@ const app = require('express');
 const router = app.Router();
 
 const {showReports, downloadExcel}  = require('../controller/reports-controller');
-
+const verifyToken = require('../middleware/verify-token-middleware');
 
 const { validateUser }  = require('../middleware/validator-middleware');
 
@@ -11,5 +11,8 @@ router.route('/')
 
 router.route('/download')
   .post(downloadExcel)
+
+router.route('/admin')
+  .post(verifyToken, showAdminReports);
 
 module.exports = router;
